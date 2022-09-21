dfc84d9fd990083b800e7af038accffb8c3f2509
takaya-okamoto <okamoto.entaku@gmail.com>
2022-09-21 17:12:39 +0900

    responsive WhatCanIDo

--- components/mainArea/whatCanIDo.tsx
+++ components/mainArea/whatCanIDo.tsx
@@ -4,9 +4,15 @@ import { Line } from '../line/line'
 export function WhatCanIDo():JSX.Element {
   return (
     <Box p="20px 0">
-      <Text pb="10px" fontWeight="bold" fontSize="4xl" color="#58595b">What can I do ?</Text>
+      <Text pb="10px" fontWeight="bold" fontSize={["3xl", "4xl", "4xl"]} color="#58595b">What can I do ?</Text>
       <Line />
-      <Box bg="#F2F0EE" borderRadius="20px" p="30px 60px">
+      <Box
+       bg="#F2F0EE"
+       borderRadius="20px"
+       pt="30px" pb="30px"
+       pr={['15px', '30px', '30px']}
+       pl={['15px', '30px', '30px']}
+      >
         <Box pb="30px">
           <Text p="10px 0" fontWeight="bold" fontSize="xl" color="#58595b">FRONTEND APPS</Text>
           <Text color="#58595b">Build client-side applications with modern features like SPA . <br/> Use modern tech such as React, Next.js,  Chakra UI . </Text>
