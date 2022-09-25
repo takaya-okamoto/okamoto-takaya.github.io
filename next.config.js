cd457a24dc878fcec151fbc4204a3684d5637339
takaya-okamoto <okamoto.entaku@gmail.com>
2022-09-25 10:27:33 +0900

    fix : config

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: process.env.GITHUB_ACTIONS ? "/portfolio-page" : "",
-  trailingSlash: true,
 }
 
 module.exports = nextConfig
