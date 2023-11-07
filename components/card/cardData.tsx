2d405990f26014e218e6be200838d7e56dfcbdb6
takaya-okamoto <okamoto.entaku@gmail.com>
2023-11-07 17:05:04 +0900

    add works

--- components/card/cardData.tsx
+++ components/card/cardData.tsx
@@ -4,17 +4,22 @@ export type CardType = {
   image: string;
   title: string;
   url: string;
-}
+};
 
 export const cardData: CardType[] = [
   {
     image: "/cardPicture/topPage.png",
     title: "Portfolio",
-    url: "#"
+    url: "#",
   },
   {
     image: "/cardPicture/prossell.png",
     title: "Corporate Page",
-    url: "https://prossell.jp/"
-  }
-]
+    url: "https://prossell.jp/",
+  },
+  {
+    image: "/cardPicture/entech.png",
+    title: "entech(Google cloud hackathon)",
+    url: "https://entech.vercel.app/",
+  },
+];
